e81b0605daa7af968ebf59d8d0bf3ca1bf1c331b
keith2018 <keith@robot9.me>
2025-11-27 18:58:12 +0800

    perf: Refactor RoPE for performance optimization

--- test/test_module.cpp
+++ test/test_module.cpp
@@ -58,21 +58,3 @@ TEST(TEST_Module, dropout) {
   EXPECT_THAT(output.shape(), ElementsAre(2, 4));
   EXPECT_TRUE((output == 0).to(DType::Float32).sum().item<float>() > 0);
 }
-
-TEST(TEST_Module, rope) {
-  auto rope = nn::RoPE(4, 3, 1000.f);
-  EXPECT_THAT(rope.cache().shape(), ElementsAre(3, 4));
-  EXPECT_TRUE(VectorNear(rope.cache().toList<float>(), {1.0000, 0.0000, 1.0000, 0.0000, 0.5403, 0.8415, 0.9995, 0.0316,
-                                                        -0.4161, 0.9093, 0.9980, 0.0632}));
-
-  auto x = Tensor(Array1d<float>{-0.0350, 0.7678, -0.4193, 0.3493, 2.0598, -0.8641, -0.7964, 2.1628,
-                                 -0.5256, 1.0712, -1.1556, 0.1891, 0.5730, 0.8307,  0.4564,  -0.2548,
-                                 -0.4788, 0.5876, -0.4538, 1.5493, 0.7187, -2.7068, -0.1677, 1.3936});
-  x.reshape_({3, 2, 4});
-  auto pos = Tensor(Array1d<int64_t>{0, 2, 1});
-  auto y = rope(x, pos);
-  EXPECT_THAT(y.shape(), ElementsAre(3, 2, 4));
-  EXPECT_TRUE(VectorNear(y.toList<float>(), {-0.0350, 0.7678, -0.4193, 0.3493, 2.0598,  -0.8641, -0.7964, 2.1628,
-                                             1.2695,  1.0571, 0.0030,  0.2564, -0.6535, 0.8452,  0.3311,  -0.2018,
-                                             0.1232,  0.5384, -0.6481, 1.5671, 0.5295,  -2.7495, 0.5142,  1.3073}));
-}
