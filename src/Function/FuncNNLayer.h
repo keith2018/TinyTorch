56c343a8c2a60aa8319e33ab06d974cda951bc65
keith2018 <keith@robot9.me>
2026-04-12 14:22:35 +0800

    refactor: remove TinyFA

--- src/Function/FuncNNLayer.h
+++ src/Function/FuncNNLayer.h
@@ -267,15 +267,6 @@ class FuncSDPAttention : public Function<FuncSDPAttention> {
   static void backward(AutogradContext* ctx, const Tensor& grad) { NOT_IMPLEMENTED(); }
 };
 
-class FuncFlashAttention : public Function<FuncFlashAttention> {
- public:
-  static Tensor forward(AutogradContext* ctx, const Tensor& query, const Tensor& key, const Tensor& value,
-                        bool isCausal) {
-    return op::flashAttention(query, key, value, isCausal);
-  }
-  static void backward(AutogradContext* ctx, const Tensor& grad) { NOT_IMPLEMENTED(); }
-};
-
 class FuncRoPE : public Function<FuncRoPE> {
  public:
   static Tensor forward(AutogradContext* ctx, const Tensor& input, const Tensor& rope, int64_t offset,
@@ -317,9 +308,6 @@ inline Tensor sdpAttention(const Tensor& query, const Tensor& key, const Tensor&
                            const Tensor& attnMask = {}, float dropoutP = 0.f,
                            std::optional<float> scale = std::nullopt) {
   return FuncSDPAttention::apply(query, key, value, isCausal, attnMask, dropoutP, scale);
-}
-inline Tensor flashAttention(const Tensor& query, const Tensor& key, const Tensor& value, bool isCausal = false) {
-  return FuncFlashAttention::apply(query, key, value, isCausal);
 }
 inline Tensor ropeApply(const Tensor& input, const Tensor& rope, int64_t offset = 0,
                         QKVLayout layout = QKVLayout::BHSD) {
